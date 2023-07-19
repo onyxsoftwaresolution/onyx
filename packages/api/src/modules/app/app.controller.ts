2653b11dc3b78624e1c5a131e9414de2b3172351
Tanase Hagi <TanaseHagi@users.noreply.github.com>
2023-07-19 22:28:40 +0300

    added allow anonymous rule for keep alive endpoint

--- packages/api/src/modules/app/app.controller.ts
+++ packages/api/src/modules/app/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AllowAnonymous } from '@modules/auth/rbac/anonymous.decorator';
 
 @Controller({
   version: '1',
@@ -21,6 +22,7 @@ export class AppController {
   }
 
   @Get('keep-alive')
+  @AllowAnonymous()
   async keepAlive() {
     await this.appService.keepAlive();
     return ({
